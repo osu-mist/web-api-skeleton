6c00075466bf7a8a6afaa3a3417aa278e04f06dc
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-06-17 12:35:59 -0700

    create codenarc groovy ruleset

---
+++ config/codenarc/codenarc.groovy
@@ -0,0 +1,17 @@
+/* CodeNarc RuleSet
+   http://codenarc.sourceforge.net/codenarc-rule-index.html */
+
+ruleset {
+    description 'MIST Groovy RuleSet'
+
+    /* class name starts with an uppercase letter and is followed by zero or
+       more word characters or dollar signs */
+    ClassName
+
+    /* method name starts with a lowercase letter */
+    MethodName
+
+    /* package name consists only of lowercase letters and numbers, separated
+       by periods */
+    PackageName
+}
