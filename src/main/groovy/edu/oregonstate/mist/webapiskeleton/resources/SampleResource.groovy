2b01f54cb6a4ea854d0e93779b56a84c69b4fcf4
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-09-08 13:02:03 -0700

    add sample post method
    co-201

--- src/main/groovy/edu/oregonstate/mist/webapiskeleton/resources/SampleResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/webapiskeleton/resources/SampleResource.groovy
@@ -2,15 +2,24 @@ package edu.oregonstate.mist.webapiskeleton.resources
 
 import edu.oregonstate.mist.webapiskeleton.core.Sample
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.Consumes
 import javax.ws.rs.core.MediaType
 
 @Path('/')
 class SampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String respond() {
+    public String getMessage() {
         new Sample().message
+    }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String postMessage(String message) {
+        message
     }
 }
