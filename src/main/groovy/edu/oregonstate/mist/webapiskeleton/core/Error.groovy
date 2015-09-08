b7b4068604fcfe96a9d7613e9b5fc7be4544e106
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-09-08 16:52:00 -0700

    reuse error representation
    co-219

---
+++ src/main/groovy/edu/oregonstate/mist/webapiskeleton/core/Error.groovy
@@ -0,0 +1,12 @@
+package edu.oregonstate.mist.webapiskeleton.core
+
+/**
+ * Error representation class.
+ */
+class Error {
+    Integer status
+    String developerMessage
+    String userMessage
+    Integer code
+    String details
+}
