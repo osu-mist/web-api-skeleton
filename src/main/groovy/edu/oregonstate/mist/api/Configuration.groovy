56c720c6bc2f90264b2cc64f6e999e561484b575
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-10-29 15:34:18 -0700

    add javadocs
    co-230

--- src/main/groovy/edu/oregonstate/mist/api/Configuration.groovy
+++ src/main/groovy/edu/oregonstate/mist/api/Configuration.groovy
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
 
+/**
+ * An object representation of the YAML configuration file.
+ */
 class Configuration extends io.dropwizard.Configuration {
     @JsonProperty('authentication')
     @NotNull
