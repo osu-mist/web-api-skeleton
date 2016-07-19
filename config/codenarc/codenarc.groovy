bc239df873bef4b05aab90ac645790ad9b1eb8a1
Andrew Ekstedt <ekstedta@oregonstate.edu>
2016-07-18 18:05:08 -0700

    Remove codenarc checks for skeleton api names
    CO-423
    
    Since we're back to the default rules for VariableName and ClassName,
    remove the rule body entirely.
    
    Also, simplify PackageName. The default rule already checks if the
    package name is lowercase, so our custom rule only needs to check
    if it begins with edu.oregonstate.mist.

--- config/codenarc/codenarc.groovy
+++ config/codenarc/codenarc.groovy
@@ -5,43 +5,18 @@ ruleset {
     description 'MIST Groovy RuleSet'
 
     /* class name starts with a uppercase letter */
-    ClassName {
-        description = 'Verify that class name starts with an uppercase letter followed by zero or ' +
-                      'more word characters or dollar signs, warning if skeleton code is unchanged.'
-        regex = '''(?x)
-                   ^(
-                     (?!(SkeletonApplication)$) # fail on skeleton class names
-                     ([A-Z]\\w*$?)*                                   # codenarc default
-                   )$'''
-        priority = 3
-    }
+    ClassName
 
     /* variable name starts with a lowercase letter */
-    VariableName {
-        description = 'Verify that class name starts with an lowercase letter followed by zero or ' +
-                'more word characters or dollar signs, warning if skeleton code is unchanged.'
-        regex = '''(?x)
-                   ^(
-                     ([a-z]\\w*$?)*
-                   )$'''
-        priority = 3
-    }
+    VariableName
 
     /* method name starts with a lowercase letter */
     MethodName
 
     PackageName {
-        description = 'Verify that package name begins with \'edu.oregonstate.mist.\' and consists ' +
-                      'only of lowercase letters and numbers separated by periods, warning if ' +
-                      'skeleton code is unchanged.'
-        regex = '''(?x)
-                   ^(
-                     edu\\.oregonstate\\.mist # begin with edu.oregonstate.mist
-                     (?!\\.webapiskeleton)    # fail on skeleton package name
-                     (\\.[a-z0-9]+)*          # periods separate lowercase alphanumeric package names
-                   )$'''
+        description = "Verify that package name begins with 'edu.oregonstate.mist.'"
+        regex = /^edu\.oregonstate\.mist\..*$/
         packageNameRequired = true
-        priority = 3
     }
 
     /* package name consists only of lowercase letters and numbers, separated
