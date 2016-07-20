dce22657f864ff34861875fab2fba4f0a28115f8
floppyears <cedenoj@onid.oregonstate.edu>
2016-07-19 17:00:27 -0700

    Misc code review fixes
    CO-440

--- src/main/groovy/edu/oregonstate/mist/api/Resource.groovy
+++ src/main/groovy/edu/oregonstate/mist/api/Resource.groovy
@@ -1,9 +1,9 @@
 package edu.oregonstate.mist.api
 
 import javax.ws.rs.core.Context
-import javax.ws.rs.core.MultivaluedMap
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.Response.ResponseBuilder
+import org.apache.http.client.utils.URIBuilder
 import javax.ws.rs.core.UriInfo
 
 /**
