5e3182edf382f4d384353ae1f4d9d5ecf1b92296
George Crary <craryg@onid.oregonstate.edu>
2016-08-25 13:53:03 -0700

    Remove Exception Handler demo
    Remove Exception Handler demo

--- src/main/groovy/edu/oregonstate/mist/api/InfoResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/api/InfoResource.groovy
@@ -26,11 +26,6 @@ class InfoResource extends Resource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getInfo(@Auth AuthenticatedUser authenticatedUser) {
-        try {
-            throw new IllegalFormatPrecisionException(3)
-        } catch(FileNotFoundException e) {
-        }
-        
         ok(info).build()
     }
 }
