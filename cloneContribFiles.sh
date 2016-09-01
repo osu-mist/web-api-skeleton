906937d8b402555abc3693e55f6b1ee38ae6295a
Jared Kosanovic <jared@kosanovj-laptop-ethernet.tss.oregonstate.edu>
2016-08-31 17:02:55 -0700

    added options to include a contrib repo

---
+++ cloneContribFiles.sh
@@ -0,0 +1,4 @@
+rm -rf src/main/groovy/edu/oregonstate/mist/contrib/
+git clone --depth=1 -q $1 src/main/groovy/edu/oregonstate/mist/contrib
+rm -rf src/main/groovy/edu/oregonstate/mist/contrib/.git/
+rm src/main/groovy/edu/oregonstate/mist/contrib/.gitignore
