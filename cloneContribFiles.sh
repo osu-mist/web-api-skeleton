f103d2f217a0d14ab7544b767f8b5c045a23864f
Jared Kosanovic <jared@kosanovj-laptop-ethernet.tss.oregonstate.edu>
2016-09-09 08:45:20 -0700

    made getContribFiles conditional

--- cloneContribFiles.sh
+++ cloneContribFiles.sh
@@ -1,4 +1,7 @@
-rm -rf src/main/groovy/edu/oregonstate/mist/contrib/
-git clone --depth=1 -q $1 src/main/groovy/edu/oregonstate/mist/contrib
-rm -rf src/main/groovy/edu/oregonstate/mist/contrib/.git/
-rm src/main/groovy/edu/oregonstate/mist/contrib/.gitignore
+if [ "$2" = "yes" ]
+    then
+        rm -rf src/main/groovy/edu/oregonstate/mist/contrib/
+        git clone --depth=1 -q $1 src/main/groovy/edu/oregonstate/mist/contrib
+        rm -rf src/main/groovy/edu/oregonstate/mist/contrib/.git/
+        rm src/main/groovy/edu/oregonstate/mist/contrib/.gitignore
+fi
