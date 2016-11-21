8e1206d94e013f8c3f65df98ab321f3904ca8885
Andrew Ekstedt <ekstedta@oregonstate.edu>
2016-11-21 11:30:59 -0800

    Add JSON pretty printing.
    CO-648
    
    Add ?pretty=true to any request to get a pretty-printed JSON response.
    Even works for error pages, which is nice.

---
+++ src/main/groovy/edu/oregonstate/mist/api/PrettyPrintResponseFilter.groovy
@@ -0,0 +1,41 @@
+package edu.oregonstate.mist.api
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.ObjectWriter
+import com.fasterxml.jackson.jaxrs.cfg.EndpointConfigBase
+import com.fasterxml.jackson.jaxrs.cfg.ObjectWriterInjector
+import com.fasterxml.jackson.jaxrs.cfg.ObjectWriterModifier
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.container.ContainerResponseContext
+import javax.ws.rs.container.ContainerResponseFilter
+import javax.ws.rs.core.MultivaluedMap
+
+@groovy.transform.TypeChecked
+public class PrettyPrintResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(
+            ContainerRequestContext requestContext,
+            ContainerResponseContext responseContext) throws IOException {
+
+        MultivaluedMap<String, String> queryParams =
+            requestContext.getUriInfo().getQueryParameters()
+
+        if (queryParams.getFirst('pretty') == 'true') {
+            ObjectWriterInjector.set(new ObjectWriterModifier() {
+                @Override
+                public ObjectWriter modify(
+                    EndpointConfigBase<?> endpoint,
+                    MultivaluedMap<String, Object> responseHeaders,
+                    Object valueToWrite,
+                    ObjectWriter w,
+                    JsonGenerator g
+                ) {
+                    g.useDefaultPrettyPrinter()
+                    w
+                }
+            })
+        }
+    }
+
+}
