1bc0fdcdbbe6076b63cd0ccc32fe5dff37f6a454
Jared Kosanovic <jared@Jareds-MacBook-Pro.local>
2016-12-06 14:29:17 -0800

    checks if page number/size is less than 0

--- src/main/groovy/edu/oregonstate/mist/api/Resource.groovy
+++ src/main/groovy/edu/oregonstate/mist/api/Resource.groovy
@@ -153,7 +153,7 @@ abstract class Resource {
      */
     protected Integer getPageNumber() {
         def pageNumber = uriInfo.getQueryParameters().getFirst('page[number]')
-        if (!pageNumber || !pageNumber.isInteger()) {
+        if (!pageNumber || !pageNumber.isInteger() || pageNumber.toInteger() < 0) {
             return DEFAULT_PAGE_NUMBER
         }
 
@@ -167,7 +167,7 @@ abstract class Resource {
      */
     protected Integer getPageSize() {
         def pageSize = uriInfo.getQueryParameters().getFirst('page[size]')
-        if (!pageSize || !pageSize.isInteger()) {
+        if (!pageSize || !pageSize.isInteger() || pageSize.toInteger() < 0) {
             return DEFAULT_PAGE_SIZE
         }
 
