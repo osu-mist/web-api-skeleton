3eeac3d322a68e5ed2bae99a4b7b8ee97977d3c6
Andrew Ekstedt <ekstedta@oregonstate.edu>
2017-02-28 14:26:32 -0800

    Add Error constructors
    CO-706
    
    Add static methods to the Error class to construct common Error values.
    Rewrite the Resource methods in terms of the new Error methods.
    
    Error is now responsible for loading the resource properties, which
    means we can get rid of Resource.loadProperties(). Error uses a static
    initializer block to load the resource properties, so there is no need
    for the application to do anything to load the error text.
    
    I considered leaving Resource.properties and Resource.loadProperties()
    in place for backwards compatibility, but I'm pretty sure none of our
    APIs use Resource.properties directly.

--- src/main/groovy/edu/oregonstate/mist/api/Error.groovy
+++ src/main/groovy/edu/oregonstate/mist/api/Error.groovy
@@ -1,5 +1,7 @@
 package edu.oregonstate.mist.api
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 /**
  * Error representation class.
  */
@@ -9,4 +11,88 @@ class Error {
     String userMessage
     Integer code
     String details
+
+    @JsonIgnore
+    private static Properties prop = new Properties()
+
+    /**
+     * Static initializer to load error text
+     * from the resource.properties file.
+     */
+    static {
+        def stream = this.getResourceAsStream('resource.properties')
+        if (stream == null) {
+            throw new IOException("couldn't open resource.properties")
+        }
+        prop.load(stream)
+        stream.close()
+    }
+
+    /**
+     * Returns a new Error for a HTTP 400 ("bad request") response.
+     *
+     * @param message the error message
+     * @return error
+     */
+    static Error badRequest(String message) {
+        new Error(
+            status: 400,
+            developerMessage: message,
+            userMessage: prop.getProperty('badRequest.userMessage'),
+            code: parseInt(prop.getProperty('badRequest.code')),
+            details: prop.getProperty('badRequest.details')
+        )
+    }
+
+    /**
+     * Returns a new Error for a HTTP 404 ("not found") response.
+     *
+     * @return error
+     */
+    static Error notFound() {
+        new Error(
+            status: 404,
+            developerMessage: prop.getProperty('notFound.developerMessage'),
+            userMessage: prop.getProperty('notFound.userMessage'),
+            code: parseInt(prop.getProperty('notFound.code')),
+            details: prop.getProperty('notFound.details')
+        )
+    }
+
+    /**
+     * Returns a new Error for a HTTP 409 ("conflict") response.
+     *
+     * @return error
+     */
+    static Error conflict() {
+        new Error(
+            status: 409,
+            developerMessage: prop.getProperty('conflict.developerMessage'),
+            userMessage: prop.getProperty('conflict.userMessage'),
+            code: parseInt(prop.getProperty('conflict.code')),
+            details: prop.getProperty('conflict.details')
+        )
+    }
+
+    /**
+     * Returns a new Error for a HTTP 500 ("internal server error") response.
+     *
+     * @param message the error message
+     * @return error
+     */
+    static Error internalServerError(String message) {
+        new Error(
+            status: 500,
+            developerMessage: message,
+            userMessage: prop.getProperty('internalServerError.userMessage'),
+            code: parseInt(prop.getProperty('internalServerError.code')),
+            details: prop.getProperty('internalServerError.details')
+        )
+    }
+
+    private static Integer parseInt(String s) {
+        if (s != null) {
+            Integer.parseInt(s)
+        }
+    }
 }
